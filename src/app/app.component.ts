979f96c6b7002258cd4e1e7b94a3fc1eefd04ce9
duong pham <phduongc1tantrao2018@gmail.com>
2024-05-06 00:20:14 +0700

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./components/login/login.component";
+import { TrangchuComponent } from "./components/trangchu/trangchu.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, LoginComponent]
+    imports: [RouterOutlet, LoginComponent, TrangchuComponent]
 })
 export class AppComponent {
   title = 'client-view';
---
+++ src/app/components/trangchu/trangchu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trangchu',
+  standalone: true,
+  imports: [],
+  templateUrl: './trangchu.component.html',
+  styleUrl: './trangchu.component.css'
+})
+export class TrangchuComponent {
+
+}
